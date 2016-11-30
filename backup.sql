c0f4350a8c4ae07026a48e654e7263b49cf5479e
John Nowinsky <armthepit@armthepit.com>
2016-11-29 19:29:56 -0600

    Revert "added code to check for duplicate signups"

--- backup.sql
+++ backup.sql
@@ -19,8 +19,6 @@
 -- Table structure for table `content_fields`
 --
 
-USE native_db;
-
 DROP TABLE IF EXISTS `content_fields`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
