3b00ed2e08e881125a939d918f28629b9f46f051
Edgar Martin del Campo <ixmati@comcast.net>
2016-11-30 03:19:33 -0600

    Note previews indicated with first-sentence preview

--- public/assets/js/youtube.js
+++ public/assets/js/youtube.js
@@ -12,19 +12,14 @@ $('.youTube').each(function (index, value) {
 	}
 });
 
-//Loop through each search result preview
-$('.youTubeFull').each(function (index, value) {
-	//Select those DIV elements without empty "" YouTube identifiers
-	if ( $(this).attr('id') !== "") {
+//Get the YouTube code from the ID value, provided through Handlebars
+var ytFullCode = $('.youTubeFull').attr('id');
 
-		//Get the YouTube code from the ID value, provided through Handlebars
-		var ytCode = $(this).attr('id');
-
-		//Form the HTML to create a YouTube thumbnail around the selected code and embed it within the current DIV
-		var ytHTML = '<iframe width="800" height="450" src="https://www.youtube.com/embed/' + ytCode + ' " frameborder="0" allowfullscreen></iframe>'
-		$('#'+ytCode).html(ytHTML);
-	}
-});
+//If the code from id isn't empty, form the HTML to create a YouTube thumbnail around the selected code and embed it within the current DIV
+if (ytFullCode !== "") {
+	var ytHTML = '<iframe width="800" height="450" src="https://www.youtube.com/embed/' + ytFullCode + ' " frameborder="0" allowfullscreen></iframe>'
+	$('.youTubeFull').html(ytHTML);
+}
 
 $('img').each(function (index,value) {
 	if ( $(this).attr('src') === "") {
@@ -32,6 +27,27 @@ $('img').each(function (index,value) {
 		$(this).hide();
 	}
 });
+
+$('.queryItem').each(function (index,value) {
+	var ifImg = $(this).children('img').attr('src');
+	var ifYouTube = $(this).children('div').attr('id');
+	//If neither the img element's src or the div element's ID is a string, the result is the preview to a written note
+	if (typeof ifImg !== 'string' && typeof ifYouTube !== 'string') {
+		$(this).prepend("<div width='250'><font style='font-size:300%'>Notes</font></div>");
+	}
+});
+
+$('.preview').each(function(index,value){
+	var previewText = $(this).html();
+	if (previewText !== "") {
+		//Add two positions from the first period, followed by a space
+		var periodPos = previewText.indexOf(". ") + 2;
+		//Add ellipses to the end of the preview sentence
+		var firstSentence = previewText.substring(0,periodPos) + "...";
+		//Insert the first sentence text into the preview text <span>
+		$(this).html(firstSentence);
+	}
+})
 
 $('.authorSpan').each(function (index,value) {
 	if ( $(this).html() === "Author: ") {
