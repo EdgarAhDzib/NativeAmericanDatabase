e8d2fb77f1262adbb6a7d93ba78e1284de7ce29d
Edgar Martin del Campo <ixmati@comcast.net>
2016-12-01 13:05:30 -0600

    Tweaked the preview section for notes entries

--- public/assets/js/checkClasses.js
+++ public/assets/js/checkClasses.js
@@ -15,7 +15,7 @@ $('.preview').each(function(index,value){
 		//Add ellipses to the end of the preview sentence
 		var firstSentence = previewText.substring(0,periodPos) + "...";
 		//Insert the first sentence text into the preview text <span>
-		$(this).html(firstSentence);
+		$(this).html("Preview:<br/>" + firstSentence);
 	}
 })
 
