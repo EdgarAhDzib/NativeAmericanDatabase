8dec940558ec74280cddb8a39aa97e07dab61c62
Edgar Martin del Campo <ixmati@comcast.net>
2016-12-02 15:09:53 -0600

    resolving merge conflict

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,10 @@
 var mysql = require('mysql');
 var connection;
-//var keys = require('./keys.js');
-
-var jawsDB = {
-		port: '3306',
-		host: 'tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-		username: 'pamv2y6gfecwynpt',
-		password: 'u52x8imwn1yenhhj',
-		database: 'q7snum71zrg8cmuw'
-	};
+var keys = require('./keys.js');
 
 //if (process.env.JAWSDB_URL) {
 	//This version of the method works on Heroku
-	connection = mysql.createConnection(jawsDB);	
+	connection = mysql.createConnection(keys.jawsDB);	
 // } else {
 // 	connection = mysql.createConnection(keys.localhost);
 // }
