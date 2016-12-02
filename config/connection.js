b7e0a353dfd8ea628d8d9552665feaa460590bdf
Edgar Martin del Campo <ixmati@comcast.net>
2016-12-01 23:17:49 -0600

    updated keys

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,18 @@
 var mysql = require('mysql');
 var connection;
-var keys = require('./keys.js');
+//var keys = require('./keys.js');
+
+var jawsDB = {
+	port: '3306',
+	host: 'sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user: 'xp5aykq1imymqn7o',
+	password: 'h7cs5fguzyq82q9h',
+	database: 'oktril0ymcyd26tt'
+};
 
 //if (process.env.JAWSDB_URL) {
 	//This version of the method works on Heroku
-	connection = mysql.createConnection(keys.jawsDB);	
+	connection = mysql.createConnection(jawsDB);	
 // } else {
 // 	connection = mysql.createConnection(keys.localhost);
 // }
