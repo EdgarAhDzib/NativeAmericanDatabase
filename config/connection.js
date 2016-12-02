cb34e9c4578f7ebf699d4700e5596c3f75b98df8
Edgar Martin del Campo <ixmati@comcast.net>
2016-12-02 04:36:12 -0600

    removed config/keys module

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,18 @@
 var mysql = require('mysql');
 var connection;
-var keys = require('./keys.js');
+//var keys = require('./keys.js');
+
+var jawsDB = {
+		port: '3306',
+		host: 'tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		user: 'pamv2y6gfecwynpt',
+		password: 'u52x8imwn1yenhhj',
+		database: 'q7snum71zrg8cmuw'
+	};
 
 //if (process.env.JAWSDB_URL) {
 	//This version of the method works on Heroku
-	connection = mysql.createConnection(keys.jawsDB);	
+	connection = mysql.createConnection(jawsDB);	
 // } else {
 // 	connection = mysql.createConnection(keys.localhost);
 // }
