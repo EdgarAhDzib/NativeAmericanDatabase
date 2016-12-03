b6688439d8e029fe28b6a61515b2bab554c289c3
Edgar Martin del Campo <ixmati@comcast.net>
2016-12-02 21:50:19 -0600

    Added functions for article POSTS and John's search function

--- controllers/app.js
+++ controllers/app.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 var models = require('../models');
 var mysql = require('mysql');
 var keys = require('../config/keys.js');
 var keyword_extractor = require("keyword-extractor");
+var passport = require('passport');
+var connection = require('../config/connection.js');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 
 //The table associations assigned through Sequelize
 models.text_contents.hasMany(models.content_fields, { foreignKey: 'content_id' });
@@ -35,7 +41,13 @@ router.post('/home', function(req, response) {
         return_changed_case: true,
         remove_duplicates: false
     });
-    console.log('I NEED TO SEARCH FOR ' + searchTerm);
+    searchQuery = "SELECT * FROM text_contents WHERE MATCH (text_contents.item_title,text_contents.main_desc,text_contents.prim_doc) AGAINST('"+searchTerm+"')";
+    connection.query(searchQuery, function(err,results){
+    	// if(err) throw err;
+    	console.log('SEARCH QUERY '+searchQuery);
+    		var handleObj = { entry: results, searchParam: true};
+    		response.render('index', handleObj);
+    })
 });
 
 router.get('/mysearches', authenticatedUser, function(req, response) {
@@ -133,61 +145,102 @@ router.get('/item/:id', function(req, response) {
 
 });
 
-/*
 //Create a variable that will be associated with the models' content
 var newItem;
 
+var reqFields = [];
+
+var userName = "";
+
 //Insert new item to text_contents table by receiving content from the posted JSON
-router.post('/item/create/', function(response){
+router.post('/item/create/', function(req, response){
 	var createDate = new Date();
 	var updateDate = new Date();
 
-//Post will have options for text, photo, or video
+	var reqMedia = req.body.media_type;
+	var reqTitle = req.body.item_title;
+	var reqGroup = req.body.group_name;
+	var reqPeriod = req.body.period;
+	var reqAuthor = req.body.author;
+	var reqNotes = req.body.notes;
+	var reqPrimDoc = req.body.prim_doc;
+	var reqUrl = req.body.url;
+	var reqPublication = req.body.publication;
+	var reqMainDesc = req.body.main_desc;
 
-//If text:
-	models.text_contents.create({
-		//Get keys from posted JSON
-		item_title: String(),
-		group:
-		period:
-		main_desc: //If content is newly written
-		prim_doc: //If the content is from already published material
-		if_published: false //Default, published TRUE after review
-		createdAt: createDate,
-		updatedAt: updateDate,
-
-//Insert related content into associated tables
-		//For source_refs table:
-		source_ref: {
-			content_id:
-			author:
-			url:
-			contributor:
-			publication:
-			createdAt: createDate,
-			updatedAt: updateDate
-		},
-		//For content_fields table (There could be several, so this will require a loop)
-		content_fields: {
-			content_id:
-			ethn_id:
-			createdAt: createDate,
-			updatedAt: updateDate
-		},
-
-	},
-	{
-		include: [
-			models.content_fields,
-			models.source_ref
-		]
+	if (req.body['ethn_fields[]']) {
+		for (i=0; i<req.body['ethn_fields[]'].length; i++) {
+			reqFields.push(req.body['ethn_fields[]'][i]);
+		}
 	}
-	).then (function(item){
-		return newItem = item;
-		response.redirect('/home');
+
+    models.user_info.find({
+            where: { id: req.user.id }
+    })
+    .then(function(results) {
+    	userName = results.name;
+    })
+    .then(function(){
+
+		if (reqMedia === "textButton") {
+			models.text_contents.create({
+
+				//Get keys from posted object
+				item_title: reqTitle,
+				group: reqGroup,
+				period: reqPeriod,
+				notes: reqNotes, //If content is newly written
+				main_desc: reqMainDesc, //If content is newly written
+				prim_doc: reqPrimDoc, //If the content is from already published material
+				if_published: false, //Default, published TRUE after review
+				createdAt: createDate,
+				updatedAt: updateDate,
+					
+				//For the source_refs table
+				source_ref: {
+					author: reqAuthor,
+					url: reqUrl,
+					contributor: userName,
+					publication: reqPublication
+				}
+
+			},
+			{
+				//INCLUDE MODELS
+				include: [models.source_ref]
+			}
+			)
+			.then (function(item){
+				newItem = item;
+				response.redirect('/home');
+			});
+			/*
+			//Add the associations for content_fields
+			.then(function(){
+				
+				models.content_fields.create({
+					//ethn_id: [EACH reqFields value]
+				})
+				.then(function(fields){
+					fields.addText_contents([newItem]);
+				});
+
+			});
+			*/
+
+	/*
+	text_contents.belongsToMany(models.content_fields, {through: 'FieldContent'});
+	*/
+
+		} // Closes the textButton condition
+
+				//For the media_sources table
+				//youtube: [GET FROM url if it's YouTube]
 	});
+}); //Closes the router function
 
-
+/*
+//Post will have options for text, photo, or video
 //If photo:
 	models.text_contents.create({
 		//Get keys from posted JSON
