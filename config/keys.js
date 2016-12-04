5032d54cf91c4ca80ee495237193cd7bef6ff0b3
Edgar Martin del Campo <ixmati@comcast.net>
2016-12-04 03:35:45 -0600

    restore heroku

---
+++ config/keys.js
@@ -0,0 +1,18 @@
+var keyObject = {
+	jawsDB: {
+		port: '3306',
+		host: 'tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		username: 'pamv2y6gfecwynpt',
+		password: 'u52x8imwn1yenhhj',
+		database: 'q7snum71zrg8cmuw'
+	},
+	localhost: {
+	    host: 'localhost',
+	    port: 3306,
+	    user: 'root',
+	    password: 'El3ctro-K@tie',
+	    database: 'native_db'
+	}
+}
+
+module.exports = keyObject;
