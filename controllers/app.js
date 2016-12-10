c2d438c5a7182c05d3e92b432bf823ef42761194
Edgar Martin del Campo <ixmati@comcast.net>
2016-12-10 01:40:01 -0600

    Saved Searches is complete

--- controllers/app.js
+++ controllers/app.js
@@ -22,8 +22,12 @@ models.text_contents.hasOne(models.source_ref, { foreignKey: 'content_id' });
 //Sequelize associations for saved searches
 models.text_contents.hasMany(models.saved_searches, {foreignKey: 'content_id'});
 models.user_info.hasMany(models.saved_searches, {foreignKey: 'user_id'});
+
 models.saved_searches.belongsTo(models.text_contents, { foreignKey: 'id' });
 models.saved_searches.belongsTo(models.user_info, { foreignKey: 'id' });
+
+// models.saved_searches.hasMany(models.media_source, {foreignKey: 'content_id'});
+// models.media_source.hasOne(models.saved_searches, {foreignKey: 'content_id'});
 
 //Initialize variables that will be assigned through the query
 var userName = "";
@@ -34,57 +38,74 @@ var handleObj;
 
 //Redirect the default path to /home
 router.get('/', function(req, response) {
-    response.redirect('/home');
+	response.redirect('/home');
 });
 
 router.get('/home', function(req, response) {
-    models.text_contents.findAll({}).then(function(data) {
-        var handleObj = { entry: data, showMenus: true };
-        response.render('index', handleObj);
-    });
+	models.text_contents.findAll({}).then(function(data) {
+		var handleObj = { entry: data, showMenus: true };
+		response.render('index', handleObj);
+	});
 });
 
 router.post('/search', function(req, response) {
 	/*
 	//Under review, this is apparently crashing the Heroku server with timeout errors
-    var searchTerm = keyword_extractor.extract(req.body.srchterm, {
-        language: "english",
-        // language: "spanish",
-        remove_digits: true,
-        return_changed_case: true,
-        remove_duplicates: false
-    });
-    */
-    var searchTerm = req.body.srchterm;
-    console.log("SEARCHTERM "+searchTerm);
-    searchQuery = "SELECT * FROM text_contents WHERE MATCH (item_title,main_desc,prim_doc) AGAINST('"+searchTerm+"')";
-    connection.query(searchQuery, function(err,results){
-    	// if(err) throw err;
-    	/*
-    	for (i=0; i<results.length; i++){
-    		console.log(results[i].id);
-    		console.log(results[i].item_title);
-    	}
-    	*/
-    	console.log('SEARCH QUERY '+searchQuery);
-    		var handleObj = { entry: results, searchValue: searchTerm, searchResults: true, searchParam: true};
-    		response.render('index', handleObj);
-    })
+	var searchTerm = keyword_extractor.extract(req.body.srchterm, {
+		language: "english",
+		// language: "spanish",
+		remove_digits: true,
+		return_changed_case: true,
+		remove_duplicates: false
+	});
+	*/
+	var searchTerm = req.body.srchterm;
+	console.log("SEARCHTERM "+searchTerm);
+	searchQuery = "SELECT * FROM text_contents WHERE MATCH (item_title,main_desc,prim_doc) AGAINST('"+searchTerm+"')";
+	connection.query(searchQuery, function(err,results){
+		// if(err) throw err;
+		/*
+		for (i=0; i<results.length; i++){
+			console.log(results[i].id);
+			console.log(results[i].item_title);
+		}
+		*/
+		console.log('SEARCH QUERY '+searchQuery);
+			var handleObj = { entry: results, searchValue: searchTerm, searchResults: true, searchParam: true};
+			response.render('index', handleObj);
+	})
 });
 
 router.get('/mysearches', authenticatedUser, function(req, response) {
-        var handleObj = { mySearches: true };
-        response.render('index', handleObj);	
+	var userId = "";
+	if (req.user) {
+		userId=req.user.id;
+	}
+	
+	models.text_contents.findAll({
+		include: [{
+			model: models.saved_searches,
+			where: { user_id: userId }
+		},
+		{
+			model: models.media_source
+		}
+		],
+	})
+	.then(function(results){
+		handleObj = { entry: results, mySearches: true };
+		response.render('index', handleObj);
+	});
 });
 
 router.get('/myarticles', authenticatedUser, function(req, response) {
-        var handleObj = { myArticles: true };
-        response.render('index', handleObj);	
+		var handleObj = { myArticles: true };
+		response.render('index', handleObj);	
 });
 
 router.get('/drafts', authenticatedUser, function(req, response) {
-        var handleObj = { drafts: true };
-        response.render('index', handleObj);	
+		var handleObj = { drafts: true };
+		response.render('index', handleObj);	
 });
 
 router.get('/about', function(req, response){
@@ -94,178 +115,205 @@ router.get('/about', function(req, response){
 
 //Show the cultures menu
 router.get('/cultures', function(req, response) {
-    models.native_group.findAll({}).then(function(data) {
-        var handleObj = { entry: data, cultures: true };
-        response.render('index', handleObj);
-    });
+	models.native_group.findAll({}).then(function(data) {
+		var handleObj = { entry: data, cultures: true };
+		response.render('index', handleObj);
+	});
 });
 
 //Show the subjects menu by tree
 router.get('/subjects/tree', function(req, response) {
-    models.ethn_fields.findAll({}).then(function(data) {
-    	var topicsArr = [];
-    	var subTopsArr = [];
+	models.ethn_fields.findAll({}).then(function(data) {
+		var topicsArr = [];
+		var subTopsArr = [];
 
-    	//Initialize a full object array that relates each subject with its main and sub-topics
-    	var fullSubjArr = [];
+		//Initialize a full object array that relates each subject with its main and sub-topics
+		var fullSubjArr = [];
 
-    	for (i=0; i<data.length; i++) {
-    		//Loops through data and pushes new value into the arrays if it doesn't already exist
-    		if (topicsArr.indexOf(data[i].main_topic) === -1){
-    			topicsArr.push(data[i].main_topic);
-    		}
-    		if (subTopsArr.indexOf(data[i].subtopic) === -1 && data[i].subtopic != "" && data[i].subtopic !== null){
-    			subTopsArr.push(data[i].subtopic);
-    		}
-    		fullSubjArr.push({main_topic:data[i].main_topic, subtopic: data[i].subtopic, subject: data[i].name});
-    	}
-    	topicsArr.sort();
-    	var subjsString = JSON.stringify(fullSubjArr);
-        var handleObj = { topic: topicsArr, allSubjs: subjsString, tree:true, subjects: true };
-        response.render('index', handleObj);
-    });
+		for (i=0; i<data.length; i++) {
+			//Loops through data and pushes new value into the arrays if it doesn't already exist
+			if (topicsArr.indexOf(data[i].main_topic) === -1){
+				topicsArr.push(data[i].main_topic);
+			}
+			if (subTopsArr.indexOf(data[i].subtopic) === -1 && data[i].subtopic != "" && data[i].subtopic !== null){
+				subTopsArr.push(data[i].subtopic);
+			}
+			fullSubjArr.push({main_topic:data[i].main_topic, subtopic: data[i].subtopic, subject: data[i].name});
+		}
+		topicsArr.sort();
+		var subjsString = JSON.stringify(fullSubjArr);
+		var handleObj = { topic: topicsArr, allSubjs: subjsString, tree:true, subjects: true };
+		response.render('index', handleObj);
+	});
 });
 
 //Show the subjects menu by alphabet
 router.get('/subjects', function(req, response) {
-    models.ethn_fields.findAll({}).then(function(data) {
-    	var namesArr = [];
-    	for (i=0; i<data.length; i++) {
-    		namesArr.push(data[i].name);
-    	}
-    	namesArr.sort();
-        var handleObj = { entry: namesArr, tree:false, subjects: true };
-        response.render('index', handleObj);
-    });
+	models.ethn_fields.findAll({}).then(function(data) {
+		var namesArr = [];
+		for (i=0; i<data.length; i++) {
+			namesArr.push(data[i].name);
+		}
+		namesArr.sort();
+		var handleObj = { entry: namesArr, tree:false, subjects: true };
+		response.render('index', handleObj);
+	});
 });
 
 router.get('/form', function(req, response) {
 
-    models.ethn_fields.findAll({}).then(function(data) {
-    	var topicsArr = [];
-    	var subTopsArr = [];
+	models.ethn_fields.findAll({}).then(function(data) {
+		var topicsArr = [];
+		var subTopsArr = [];
 
-    	//Initialize a full object array that relates each subject with its main and sub-topics
-    	var fullSubjArr = [];
+		//Initialize a full object array that relates each subject with its main and sub-topics
+		var fullSubjArr = [];
 
-    	for (i=0; i<data.length; i++) {
-    		//Loops through data and pushes new value into the arrays if it doesn't already exist
-    		if (topicsArr.indexOf(data[i].main_topic) === -1){
-    			topicsArr.push(data[i].main_topic);
-    		}
-    		if (subTopsArr.indexOf(data[i].subtopic) === -1 && data[i].subtopic != "" && data[i].subtopic !== null){
-    			subTopsArr.push(data[i].subtopic);
-    		}
-    		fullSubjArr.push({main_topic:data[i].main_topic, subtopic: data[i].subtopic, subject: data[i].name});
-    	}
-    	topicsArr.sort();
-    	var subjsString = JSON.stringify(fullSubjArr);
-        var handleForm = { topic: topicsArr, allSubjs: subjsString, showForm: true };
-        response.render('index', handleForm);
-    });
+		for (i=0; i<data.length; i++) {
+			//Loops through data and pushes new value into the arrays if it doesn't already exist
+			if (topicsArr.indexOf(data[i].main_topic) === -1){
+				topicsArr.push(data[i].main_topic);
+			}
+			if (subTopsArr.indexOf(data[i].subtopic) === -1 && data[i].subtopic != "" && data[i].subtopic !== null){
+				subTopsArr.push(data[i].subtopic);
+			}
+			fullSubjArr.push({main_topic:data[i].main_topic, subtopic: data[i].subtopic, subject: data[i].name});
+		}
+		topicsArr.sort();
+		var subjsString = JSON.stringify(fullSubjArr);
+		var handleForm = { topic: topicsArr, allSubjs: subjsString, showForm: true };
+		response.render('index', handleForm);
+	});
 
 });
 
 router.get('/authenticated', authenticatedUser, function(req, response) {
-    response.render('authenticated');
+	response.render('authenticated');
 });
 
 router.get('/subj/:categ', function(req, response) {
-    var categ = req.params.categ;
+	var categ = req.params.categ;
 
-    models.text_contents.findAll({
-            include: [{
-                model: models.content_fields,
-                where: { ethn_id: categ }
-            }, {
-                model: models.media_source
-            }],
-        })
-        .then(function(results) {
-            var handleObj = { entry: results, isSubj: true, searchParam: true };
-            response.render('index', handleObj);
-        });
+	models.text_contents.findAll({
+			include: [{
+				model: models.content_fields,
+				where: { ethn_id: categ }
+			}, {
+				model: models.media_source
+			}],
+		})
+		.then(function(results) {
+			var handleObj = { entry: results, isSubj: true, searchParam: true };
+			response.render('index', handleObj);
+		});
 
 });
 
 router.get('/group/:groupname', function(req, response) {
-    var groupname = req.params.groupname;
+	var groupname = req.params.groupname;
 
-    models.text_contents.findAll({
-            where: { group: groupname },
-            include: [{
-                model: models.media_source
-            }],
-        })
-        .then(function(results) {
-            var handleObj = { entry: results, isGroup: true, searchParam: true };
-            response.render('index', handleObj);
-        });
+	models.text_contents.findAll({
+			where: { group: groupname },
+			include: [{
+				model: models.media_source
+			}],
+		})
+		.then(function(results) {
+			var handleObj = { entry: results, isGroup: true, searchParam: true };
+			response.render('index', handleObj);
+		});
 
 });
 
 //Get the item according to its text_contents ID number
 router.get('/item/:id', function(req, response) {
-    var itemId = req.params.id;
-    var userId = "";
-    if (req.user) {
-    	userId=req.user.id;
-    }
-    console.log("USER ID is " + userId);
+	var itemId = req.params.id;
+	var userId = "";
+	if (req.user) {
+		userId=req.user.id;
+	}
+	console.log("USER ID is " + userId);
 
-    models.text_contents.findAll({
-            where: { id: itemId },
-            include: [{
-                model: models.content_fields
-            }, {
-                model: models.media_source
-            }, {
-                model: models.source_ref
-            }, {
-            	//This inclusion will check whether the item is already saved for the user - and thus whether to remove it
-            	model: models.saved_searches
-            }
-            ],
-        })
-        .then(function(results) {
-            handleObj = { entry: results, itemPage: true, isSaved: false };
+	models.text_contents.findAll({
+			where: { id: itemId },
+			include: [{
+				model: models.content_fields
+			}, {
+				model: models.media_source
+			}, {
+				model: models.source_ref
+			}, {
+				//This inclusion will check whether the item is already saved for the user - and thus whether to remove it
+				model: models.saved_searches
+			}
+			],
+		})
+		.then(function(results) {
+			handleObj = { entry: results, itemPage: true, isSaved: false };
 
-        	for (i=0; i<results.length; i++){
-        		currId = results[i].dataValues.id;
+			for (i=0; i<results.length; i++){
+				currId = results[i].dataValues.id;
 
-        		if (results[i].saved_searches.length > 0) {
+				if (results[i].saved_searches.length > 0) {
 
-        			//Match the item for logged in user and ID in saved_searches table
-	        		if (userId === results[i].saved_searches[0].dataValues.user_id && currId === results[i].saved_searches[0].dataValues.content_id) {
+					//Match the item for logged in user and ID in saved_searches table
+					if (userId === results[i].saved_searches[0].dataValues.user_id && currId === results[i].saved_searches[0].dataValues.content_id) {
 
-	        			//Then toggle button to indicate it is already saved for current user
-	        			handleObj = { entry: results, itemPage: true, isSaved: true };
-	        		}
-        		}
-        	}
-        	//Moved the render from this scope to await completion of conditional logic        	
-        }).then(function(){
-        	response.render('index', handleObj);
-        });
+						//Then toggle button to indicate it is already saved for current user
+						handleObj = { entry: results, itemPage: true, isSaved: true };
+					}
+				}
+			}
+			//Moved the render from this scope to await completion of conditional logic        	
+		}).then(function(){
+			response.render('index', handleObj);
+		});
 
 });
 
 //Save an item to Searches by posting with its ID number
 router.post('/item/save', function(req,response){
-	var saveItem = req.body.item_id;
-    var userId = "";
-    var createDate = new Date();
+	var saveItem = req.body.item_id.trim();
+	var userId = "";
+	var createDate = new Date();
 	var updateDate = new Date();
 	
 	//Insert record only if user is logged in    
-    if (req.user) {
-    	userId=req.user.id;
-    	models.saved_searches.create({
-    		user_id: userId,
-    		content_id: saveItem
-    	});
-    }
+	if (req.user) {
+		userId=req.user.id;
+		models.saved_searches.create({
+			user_id: userId,
+			content_id: saveItem
+		})
+		.then(function() {
+			response.redirect('/item/'+saveItem);
+		});
+	}
 
+});
+
+//Remove an item from Searches by posting with its ID number
+router.post('/item/remove', function(req,response){
+	var removeItem = req.body.item_id;
+	var userId = "";
+	
+	//Remove record only if user is logged in and item has a match
+	if (req.user) {
+		userId=req.user.id;
+		models.saved_searches.destroy({
+			where: {
+				user_id: userId,
+				content_id: removeItem
+			}
+		})
+		.then(function() {
+			response.redirect('/savedsearches');
+		});
+	}
+});
+
+router.get('/savedsearches', function(req, response){
+	response.redirect('/mysearches');
 });
 
 //Create a variable that will be associated with the models' content
@@ -299,13 +347,13 @@ router.post('/item/create/', function(req, response){
 		}
 	}
 
-    models.user_info.find({
-            where: { id: req.user.id }
-    })
-    .then(function(results) {
-    	userName = results.name;
-    })
-    .then(function(){
+	models.user_info.find({
+			where: { id: req.user.id }
+	})
+	.then(function(results) {
+		userName = results.name;
+	})
+	.then(function(){
 
 		if (reqMedia === "textButton") {
 			models.text_contents.create({
@@ -464,12 +512,12 @@ router.post('/item/create/', function(req, response){
 }); //Closes the router function
 
 function authenticatedUser(req, response, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    } else {
-        req.flash('error_msg', 'You are not logged in.');
-        response.redirect('/users/login');
-    }
+	if (req.isAuthenticated()) {
+		return next();
+	} else {
+		req.flash('error_msg', 'You are not logged in.');
+		response.redirect('/users/login');
+	}
 }
 
 module.exports = router;
--- public/assets/js/saveSearch.js
+++ public/assets/js/saveSearch.js
@@ -1,6 +1,6 @@
 var itemID;
 
-$("#saveItem").find('button').on("click", function(){
+$('#saveItem').find('button').on("click", function(){
 
 	//Get the value of the item ID for posting
 	itemID = $(this).attr('id');
@@ -11,6 +11,21 @@ $("#saveItem").find('button').on("click", function(){
 
 	//This will require validation
 	$.post("../item/save/", contentObj, function(){
+	});
+
+});
+
+$('.removeItem').find('button').on("click", function(){
+
+	//Get the value of the item ID for posting
+	itemID = $(this).attr('id');
+
+	var contentObj = {
+		item_id: itemID
+	};
+
+	//This will require validation
+	$.post("../item/remove/", contentObj, function(){
 	});
 
 });
